c6cd14ea0ff1ede16499912c22b3aae55f79223c
vincentezw <vincent.zwanenburg@shopify.com>
2021-09-28 15:39:58 +0100

    typo

--- module1/first_practice.cpp
+++ module1/first_practice.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-// doubleNumber takes a number and return double its value
+// doubleNumber takes a number and returns double its value
 int doubleNumber(int num) {
   return num * 2;
 }
 
 int main(int argc, char** argv) {
-  int x = 2;
+  int x = 1;
   while (x <= 64) {
     int y = doubleNumber(x);
     cout << x << ", " << y << endl;
